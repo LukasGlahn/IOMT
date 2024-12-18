0a8e8ec88a15c9baa9bc549572344ea86a2e9aa9
scotthvid <148949305+scotthvid@users.noreply.github.com>
2024-12-18 11:49:44 +0100

    IRsensor kode

---
+++ Device/IRlyssensor.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+# Sensor Digital Pin Configuration
+pin_sensor_digital = 24  # flammesensor
+pin_led = 23  # Powerled
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
+GPIO.setup(pin_sensor_digital, GPIO.IN)  # Flammesensor bliver sat som input
+GPIO.setup(pin_led, GPIO.OUT)  # LED bliver sat som output
+
+try:
+    # Main loop
+    while True:
+        sensor_value_digital = GPIO.input(pin_sensor_digital)
+
+        if sensor_value_digital == 1:
+            GPIO.output(pin_led, GPIO.HIGH)  # Sluk LED
+        else:
+            GPIO.output(pin_led, GPIO.LOW)  # Tænd LED
+except KeyboardInterrupt:
+    print("Kode afsluttet")
+finally:
+    GPIO.cleanup()  # Nulstiller GPIO states
